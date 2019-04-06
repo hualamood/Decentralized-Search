fdd710f1e5f21adadc3a492136f953f30b7b4ada
Hussain Alamood <hualamood@gmail.com>
2019-04-06 13:46:05 +0200

    Bootstrap to multiple nodes

--- base/search-engine/index.js
+++ base/search-engine/index.js
@@ -1,7 +1,14 @@
 const sharer = require('./src/sharer.js')
 const node = new sharer('discovery', 'bootstrap',['api_server', 9009])
 
-node.Bootstrap('/dns4/ipfs.io/ipfs/Qmaisz6NMhDB51cCvNWa1GMS7LU1pAxdF4Ld6Ft9kZEP2a').then((res) => {
-  console.log(res);
+
+const bootstrap_servers = ["/dnsaddr/bootstrap.libp2p.io/ipfs/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN",
+                        "/dnsaddr/bootstrap.libp2p.io/ipfs/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa",
+                        "/dnsaddr/bootstrap.libp2p.io/ipfs/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb",
+                        "/dnsaddr/bootstrap.libp2p.io/ipfs/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt",
+                        "/ip6/2a05:f6c2:5050:0:6:28f3:b213:2fc3/tcp/4001/ipfs/QmZcGvWL6UiZL4Qhy6YGtWeDsj8RdaWDhFtPHZj83XaJsc"]
+
+node.Bootstrap(bootstrap_servers).then((res) => {
+  console.log("Bootstrapped", res);
+  node.InitNode()
 })
-node.InitNode()
--- base/search-engine/src/sharer.js
+++ base/search-engine/src/sharer.js
@@ -8,6 +8,7 @@ module.exports = class Communicator {
   constructor(discovery, ipfs_addr, server_addr) {
     this.pair_hash = ''
     this.discovery = discovery
+    this.bootstrap_servers = bootstrap_servers
 
     this.ipfs = IpfsAPI(ipfs_addr, '5001')
     this.getter = new Getter(server_addr)
@@ -37,6 +38,25 @@ module.exports = class Communicator {
       }
     }
   }
+
+  Bootstrap(servers) {
+    return new Promise((resolve, reject) => {
+      servers.forEach((server) => {
+        if ((! server.includes('127.0.0.1')) && server.includes('/ipfs/')) {
+          this.ipfs.bootstrap.add(server, (err, res) => {
+            if (err) {
+              reject(err);
+            } else {
+              if (server == servers[servers.length-1]) {
+                resolve({ peers: servers })
+              }
+            }
+          })
+        }
+      })
+    });
+  }
+
 
   Index(msg) {
     this.ipfs.get(msg['ipfs_hash'], (err, files) => {
@@ -78,17 +98,17 @@ module.exports = class Communicator {
     });
   }
 
-  Bootstrap(addr) {
-    return new Promise((resolve, reject) => {
-      this.ipfs.bootstrap.add(addr, (err, res) => {
-        if (err) {
-          reject(err)
-        } else {
-          resolve(res)
-        }
-      })
-    })
-  }
+  // Bootstrap(addr) {
+  //   return new Promise((resolve, reject) => {
+  //     this.ipfs.bootstrap.add(addr, (err, res) => {
+  //       if (err) {
+  //         reject(err)
+  //       } else {
+  //         resolve(res)
+  //       }
+  //     })
+  //   })
+  // }
 
   InitNode() {
     this.ipfs.id().then((id) => {
